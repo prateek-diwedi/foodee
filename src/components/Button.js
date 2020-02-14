75b883d9128ec91f8c2e06aec42850fe2035425b
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-13 16:38:42 -0800

    added buttons to storybook

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Button.scss";
+import classnames from "classnames"
+
+export default function Button(props) {
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+  
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
