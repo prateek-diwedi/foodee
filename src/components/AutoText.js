d11124bdb7415ea2203147cabf8840282d812477
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-15 14:00:39 -0800

    added typwriter effect for text

---
+++ src/components/AutoText.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AutoText.scss"
+
+export default function AutoText() {
+
+  return (
+    <h1>What are you looking for ..? </h1>
+    <h1 data-text="Breakfast, Lunch, Dinner ">
+     Breakfast, Lunch, Dinner 
+    </h1>
+  )
+}
