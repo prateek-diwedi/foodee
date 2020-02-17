d4c46f4f5e8317040e20e0aa83cf0efab767c500
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-02-16 16:54:10 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
 //import FirstPage from "./components/firstPage"
 import ThirdPage from "./thirdpage"
-=======
 import FirstPage from "./components/firstPage"
 
->>>>>>> master
 
 function App() {
   return (
     <div className="App">
       <div>
-<<<<<<< HEAD
       {/* <FirstPage></FirstPage> */}
       <ThirdPage></ThirdPage>
-=======
       <FirstPage></FirstPage>
->>>>>>> master
     </div>
     </div>
   );
