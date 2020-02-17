e8917964fe7f2ad68a9f293e14cd438a78ceea31
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-16 17:05:51 -0800

    auto  text position fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div>
       {/* <FirstPage></FirstPage> */}
-      <ThirdPage></ThirdPage>
+      {/* <ThirdPage></ThirdPage> */}
       <FirstPage></FirstPage>
     </div>
     </div>
