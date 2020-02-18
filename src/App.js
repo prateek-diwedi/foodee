c0e93d9a9e04266adaf85ba5dc091657dea3fb2b
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-17 20:14:14 -0800

    routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import FirstPage from "./components/firstPage";
+import axios from 'axios'
 
 function App() {
+  const [message, setMessage] = useState("NO MESSAGE YET")
   return (
     <div>
        
       <FirstPage></FirstPage>
+
+      <button onClick={() => {
+        axios.get('/api/dogs')
+          .then(res => setMessage(res.data.message))
+      }}>CLICK</button>
+      <h1>{message}</h1>
       
     </div>
   );
