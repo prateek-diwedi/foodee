5ec8ec434cad5b9e889c87e374eb82d823aa4d22
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-02-18 16:58:52 -0800

    Added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,51 +1,24 @@
+
 import React from "react";
-import "./NavBar.scss"
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 
 export default function NavBar() {
 
   return (
-    <div class="html-container">
-      <div class="html-section">
-        <div class="html-project">
-          <div class="navigation">
-            <nav>
-              <ul class="nav-type">
-                <li><a href="https://codepen.io/EneergeticTomy/" target="_blank" class="active">Logo</a></li>
-                <li><a href="https://codepen.io/TomikaBoy/full/LqMpaE" target="_blank" class="active1">Location</a></li>
-                <li><a href="https://codepen.io/" target="_blank" class="active2">Contact</a></li>
-                <li><a  target="_blank" class="active3">Login</a></li>
-                <div class="line"></div>
-                <li><a><i class="fa fa-search" type="text" placeholder="Search...">
-                <div class="search"></div>
-                  </i></a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-      <noscript>
-      <div class="first-line">
-        <div class="linear">
-          <div class="line-w5">
-            
-          </div>
-        </div>
-      </div>
-      </noscript>
-      <template>
-        <div class="flash-fluid">
-          <div class="flash-time">
-            <div class="flash-GIF">
-            </div>
-          </div>
-       </div>
-      </template>
-      <noscript>     
-        <div class="cyrcle-center">
-          <div class="radial-cyrcle">
-          </div>
-        </div>
-      </noscript>
-    </div>
+    <div>
+    <Navbar bg="danger" variant="dark">
+    <Navbar.Brand href="/">Fooddee</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/signIn">Login</Nav.Link>
+      <Nav.Link href="#features">Location</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+
+  </div>
   )
 }
+
