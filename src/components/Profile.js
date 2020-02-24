848065198a1caec0309cbdc5aa7adeca5277a94b
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-02-23 16:20:09 -0800

    added profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,6 +9,7 @@ export default function Profile() {
     <h3>Logged in as name</h3>
     <h3>email : </h3>
     <h2> Your favourite restuarents are: </h2>
+    <SearchDescription></SearchDescription>
     
     </div>
   )
