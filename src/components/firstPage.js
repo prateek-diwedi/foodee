0cc354aea3acee3c18a95396d6fb32b31c4783d2
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-24 20:11:07 -0800

    cookies working now

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Button from "../components/Button";
-import SearchBar from "../components/SearchBar";
+// import Button from "../components/Button";
+// import SearchBar from "../components/SearchBar";
 import PictureWithInfo from "../components/PictureWithInfo";
 import PictureWithInfoReverse from "../components/PictureWithInfoRevrese"
 import Footer from "../components/Footer"
 import WelcomePage from "../components/WelcomePage"
-import AutoText from "../components/AutoText"
+// import AutoText from "../components/AutoText"
 import Categories from "../components/Categories"
-import Location from "../components/LocationWithCoordinates"
+// import Location from "../components/LocationWithCoordinates"
 
 
 export default function FirstPage() {
