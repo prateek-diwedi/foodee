d40d220dd58e14c14d9ef92d13157fdbaa963fd3
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-02-26 15:59:53 -0800

    Added profile and share button page

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -1,8 +1,5 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-// import './index.css';
 import { Modal, Icon } from 'antd';
 import ShareButton from "./ShareButtons"
 
@@ -20,38 +17,34 @@ export class ShareModal extends React.Component {
   };
 
   handleOk = e => {
-    console.log(e);
     this.setState({
       visible: false,
     });
   };
 
   handleCancel = e => {
-    console.log(e);
     this.setState({
       visible: false,
     });
   };
 
   render() {
-    console.log('props in share modal--->', this.props)
     return (
       <div>
-         <Icon type="share-alt" 
-        style={{ fontSize: "40px", color: "#08c" }}
-        onClick={this.showModal}/>
+        <Icon type="share-alt"
+          style={{ fontSize: "40px", color: "#08c" }}
+          onClick={this.showModal} />
         <Modal
           title="Share This Place With Your Friends!"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
         >
-          <ShareButton  url={this.props.rst_id}/>
+          <ShareButton url={this.props.rst_id} loc={this.props.loc} />
         </Modal>
       </div>
     );
   }
 }
 
-export default ShareModal
-// ReactDOM.render(<App />, document.getElementById('container'));
+export default ShareModal;
--- src/components/description.js
+++ src/components/description.js
@@ -5,84 +5,72 @@ import { Rate } from "antd";
 import MapDirection from "./directionMap";
 import MapModal from "./mapmodal";
 import { Icon } from "antd";
-// import Rating from "./Rating";
 import ShareModal from "../components/ShareModal";
 
 const axios = require("axios");
-
-
 const divStyle = {
   paddingLeft: "55px",
   paddingTop: "10px"
 };
 const Description = props => {
   const [theme, setTheme] = useState(false);
-  const [saved,setSaved] = useState(false);
-  const [id,setID] = useState(null);
-  console.log(props.user)
-
+  const [saved, setSaved] = useState(false);
+  const [id, setID] = useState(null);
   let user_id = props.user.user_id;
-  let res_id= props.restaurant.id;
-  //  }
-// First the page reads the favourite from table if it was liked before to change it to true
-useEffect(()=>{
-  axios.get("http://localhost:3001/find_favourite",
-  {params: {user_id:user_id,
-            res_id :res_id}})
-  .then(response=>{
-    // console.log(favourite)
-    console.log('---->>>>>>>>>>>',response.data)
-    if (response.data){
-      setTheme(!theme);
-      setSaved(!saved);
-      setID(response.data.id);
-    }
-  })
-  .catch(e=>console.log(e))
-},[])
-  
-console.log(`theme changed to ${theme} and saved changed to ${saved}`);
+  let res_id = props.restaurant.id;
+  let loc = props.user_loc
+
+  // First the page reads the favourite from table if it was liked before to change it to true
+  useEffect(() => {
+    axios.get("http://localhost:3001/find_favourite",
+      {
+        params: {
+          user_id: user_id,
+          res_id: res_id
+        }
+      })
+      .then(response => {
+        if (response.data) {
+          setTheme(!theme);
+          setSaved(!saved);
+          setID(response.data.id);
+        }
+      })
+      .catch(e => console.log(e))
+  }, [])
 
   const handleliked = () => {
-    console.log(id)
-     let favourite = {
-       id: id,
+    let favourite = {
+      id: id,
       user_id: props.user.user_id,
-      res_id: props.restaurant.id 
-     }
-     console.log('favourite',favourite)
-     if (saved){
-       // need to DELTE 
-       axios
-       .delete(`http://localhost:3001/favourites`, 
-       {params: {id:id}})
+      res_id: props.restaurant.id
+    }
+    if (saved) {
+      // need to DELTE 
+      axios
+        .delete(`http://localhost:3001/favourites`,
+          { params: { id: id } })
         .then(response => {
-         setTheme(!theme);
-         setSaved(!saved);
-        //  setID(id);
-           console.log(`theme changed to ${theme} and saved changed to ${saved}`);
-           console.log(`DELETE response is ${response}`)
-         })
-         .catch(e=>{console.log(e)})
-     } else {
-       // need to POST
-       axios
-       .post("http://localhost:3001/favourites", 
-        {favourite}
-       )
+          setTheme(!theme);
+          setSaved(!saved);
+          //  setID(id);
+        })
+        .catch(e => { console.log(e) })
+    } else {
+      // need to POST
+      axios
+        .post("http://localhost:3001/favourites",
+          { favourite }
+        )
         .then(response => {
-         setTheme(!theme);
-         setSaved(!saved);
-         setID(response.data.favourite.id);
-         console.log(`theme changed to ${theme} and saved changed to ${saved}`);
-         console.log(response.data)
-         })
-         .catch(e=>{console.log(e)})
-     }
+          setTheme(!theme);
+          setSaved(!saved);
+          setID(response.data.favourite.id);
+        })
+        .catch(e => { console.log(e) })
+    }
   };
 
-  
- 
   return (
     <div style={divStyle}>
       <Row>
@@ -125,7 +113,7 @@ console.log(`theme changed to ${theme} and saved changed to ${saved}`);
       <Row>
         <Col md="auto">
           <Icon
-           theme = {theme? 'filled':'outlined'}
+            theme={theme ? 'filled' : 'outlined'}
             type="like"
             style={{ fontSize: "40px", color: "#08c" }}
             onClick={handleliked}
@@ -137,22 +125,15 @@ console.log(`theme changed to ${theme} and saved changed to ${saved}`);
             <MapDirection
               start_lng={props.user_loc.lan}
               start_lat={props.user_loc.lat}
-              // start_lat={49.2813} 
-              // start_lng={-123.1151} 
               end_lng={props.location.longitude}
               end_lat={props.location.latitude}
             />
           </MapModal>
         </Col>
         <Col md="auto">
-          <ShareModal rst_id={props.restaurant.id}/>
+          <ShareModal rst_id={props.restaurant.id} loc={loc} />
         </Col>
       </Row>
-      {/* <Row>
-        <Rating restaurant = {props.restaurant} user = {props.user}>
-
-        </Rating>
-      </Row> */}
     </div>
   );
 };
